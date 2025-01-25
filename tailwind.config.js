2bd98dd529ad1a505378242de9d9b360e408117f
Edwin Vazquez Vega <120763821+Revokeez@users.noreply.github.com>
2025-01-25 01:54:52 -0400

    Added animation intailwind.config for text affect in activity.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,35 @@ module.exports = {
         'infotech-white': '#fffdfa',
         'infotech-black': '#333333',
       },
-    },
+      animation: {
+        typewriter: 'typewriter 2s steps(11) forwards',
+        caret: 'typewriter 2s steps(11) forwards, blink 1s steps(11) infinite 2s',
+      },
+      keyframes: {
+        typewriter: {
+          to: {
+            left: '100%',
+          },
+        },
+        blink: {
+          '0%': {
+            opacity: '0',
+          },
+          '0.1%': {
+            opacity: '1',
+          },
+          '50%': {
+            opacity: '1',
+          },
+          '50.1%': {
+            opacity: '0',
+          },
+          '100%': {
+            opacity: '0',
+          },
+        }
+      }
+    }
   },
   plugins: [],
 }
