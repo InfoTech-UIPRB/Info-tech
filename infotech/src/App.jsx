69fd49455954def2011302aaaa7ab815e76caa45
Edwin Vazquez Vega <120763821+Revokeez@users.noreply.github.com>
2025-04-26 11:55:20 -0300

    Making Carousel. It can change image but not automaticly

--- infotech/src/App.jsx
+++ infotech/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
 
   return (
     <>
-       <Button variant="contained">Hello world</Button>
+      <HomePage />
     </>
   )
 }
--- infotech/src/pages/HomePage.jsx
+++ infotech/src/pages/HomePage.jsx
@@ -1,22 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 //Components
 
 
-const SlideShow = () => {
-    return (
-        <>
-            <img src="Chelimar_dando_algo.jpg" alt="..." />
-            <img src="Foto_de_Actividad_del_sum_con_director.jpg" alt="..." />    
-        </>
-
-
-    )
-}
-
 const HomePage = () => {
+
+    const images = ["Chelimar_dando_algo.jpg", "Foto_de_Actividad_del_sum_con_director.jpg"]
+    const [index, setIndex] = useState(0);
+    const length = images.length;
+
+    const previous = () => {
+        const newIndex = index -1
+        setIndex(newIndex < 0 ? length -1 : newIndex);
+    }
+
+    const next = () => {
+        const newIndex = index + 1
+        setIndex(newIndex >= length ? 0 : newIndex)
+    }
     return (
-<h1>Hey!</h1>
+        <div className="border-4">
+            <button onClick={previous}>Previous </button>
+            <button onClick={next}>Next</button>
+            <img src={images[index]} />
+        </div>
     )
 }
  export default HomePage;
