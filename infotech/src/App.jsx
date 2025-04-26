23c073f4d5b82dde16698750dc0464a606a1f50a
Edwin Vazquez Vega <120763821+Revokeez@users.noreply.github.com>
2025-04-26 11:09:52 -0300

    Remove flobite and flowbite-react. Reason: Carousel wasnt working and didnt find any solution.  https://github.com/themesberg/flowbite-react/discussions/801.

--- infotech/src/App.jsx
+++ infotech/src/App.jsx
@@ -1,5 +1,4 @@
-import React from 'react' 
-
+import React from 'react' ;
 
 //Pages
 import HomePage from './pages/HomePage.jsx'
--- infotech/src/pages/HomePage.jsx
+++ infotech/src/pages/HomePage.jsx
@@ -1,29 +1,22 @@
 import React from "react";
 
 //Components
-import NavBar from "../components/NavBar.jsx";
 import { Carousel } from "flowbite-react";
 
-//Images
-// import photo01 from "../assets/Images/Chelimar_dando_algo.jpg";
-// import photo02 from "../assets/Images/Foto_de_Actividad_del_sum_con_director.jpg";
 const SlideShow = () => {
     return (
-        <Carousel>            
+        <>
             <img src="Chelimar_dando_algo.jpg" alt="..." />
-            <img src="Foto_de_Actividad_del_sum_con_director.jpg" alt="..." />
+            <img src="Foto_de_Actividad_del_sum_con_director.jpg" alt="..." />    
+        </>
 
-             {/*<img src="https://flowbite.com/docs/images/carousel/carousel-4.svg" alt="..." />
-            <img src="https://flowbite.com/docs/images/carousel/carousel-5.svg" alt="..." /> */}
-        </Carousel>
+
     )
 }
 
 const HomePage = () => {
     return (
-        <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
-            <SlideShow />
-        </div>
+<h1>Hey!</h1>
     )
 }
  export default HomePage;
