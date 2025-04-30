44968a65c37db3c9121fe15895b3be223f93b6e7
Revok <revokeezjn@gmail.com>
2025-04-30 12:24:56 -0400

    Change url in 404Page.jsx

--- infotech/src/pages/404Page.jsx
+++ infotech/src/pages/404Page.jsx
@@ -17,7 +17,7 @@ const Page404 = () => {
                     <p className="mt-6 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8">
                       Sorry, we couldn’t find the page you’re looking for.
                     </p>
-                    <Link to="/pages">
+                    <Link to="/">
                         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full mt-4" > 
                             Go to Home Page
                         </button>
