c7c67e62a5bd0aeb59f8ad25e9556e2589a91a3c
Revok <revokeezjn@gmail.com>
2025-05-03 18:13:38 -0400

    Added links in Social Page. It still not in use

--- infotech/src/pages/Socials.jsx
+++ infotech/src/pages/Socials.jsx
@@ -8,13 +8,13 @@ const Socials = () => {
     {
       name: "Instagram",
       icon: <InstagramIcon size={40} />,
-      url: "https://instagram.com/yourpage",
+      url: "https://www.instagram.com/infotech.uiprb/",
       color: "text-pink-500"
     },
     {
       name: "Github",
       icon: <GithubIcon size={40} />,
-      url: "https://discord.gg/yourserver",
+      url: "https://github.com/InfoTech-UIPRB",
       color: "text-indigo-500"
     },
   ];
@@ -25,14 +25,14 @@ const Socials = () => {
         <div className="flex md:flex-row flex-col items-center justify-center gap-8 mt-5">
             {socials.map((social, idx) => (
                     <a
-                    key={idx}
-                    href={social.url}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className={`flex flex-col items-center p-6 bg-white bg-opacity-10 rounded-xl hover:scale-105 transition transform duration-300 w-50`}
-                    >
-                    <div className={`${social.color} mb-2`}>{social.icon}</div>
-                    <span className="text-xl font-semibold text-black">{social.name}</span>
+                        key={idx}
+                        href={social.url}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className={`flex flex-col items-center p-6 bg-white bg-opacity-10 rounded-xl hover:scale-105 transition transform duration-300 w-50`}
+                        >
+                        <div className={`${social.color} mb-2`}>{social.icon}</div>
+                        <span className="text-xl font-semibold text-black">{social.name}</span>
                     </a>
             ))}
         </div>
