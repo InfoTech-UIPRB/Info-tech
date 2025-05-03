b83151b118e15d205d5d86e36f35ba8a47b9a189
Revok <revokeezjn@gmail.com>
2025-05-03 18:03:21 -0400

    Added Social page. But its not in use

--- infotech/src/components/Footer.jsx
+++ infotech/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import InfotechLogo from "../assets/Images/png_logo_infotech.png"
 
 const Footer = () => {
     return (
-        <footer className=" text-gray-300 py-10">
+        <footer className="text-gray-300 py-10">
           <div className="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8">
             <div className="flex flex-wrap justify-between">
               {/* <!-- Logo and Description --> */}
---
+++ infotech/src/pages/Socials.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { InstagramIcon, LinkedinIcon, GithubIcon } from "lucide-react";
+import NavBar from "../components/Navbar";
+import Footer from "../components/Footer";
+
+const Socials = () => {
+  const socials = [
+    {
+      name: "Instagram",
+      icon: <InstagramIcon size={40} />,
+      url: "https://instagram.com/yourpage",
+      color: "text-pink-500"
+    },
+    {
+      name: "Github",
+      icon: <GithubIcon size={40} />,
+      url: "https://discord.gg/yourserver",
+      color: "text-indigo-500"
+    },
+  ];
+
+  return (
+    <>
+      <NavBar />
+        <div className="flex md:flex-row flex-col items-center justify-center gap-8 mt-5">
+            {socials.map((social, idx) => (
+                    <a
+                    key={idx}
+                    href={social.url}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className={`flex flex-col items-center p-6 bg-white bg-opacity-10 rounded-xl hover:scale-105 transition transform duration-300 w-50`}
+                    >
+                    <div className={`${social.color} mb-2`}>{social.icon}</div>
+                    <span className="text-xl font-semibold text-black">{social.name}</span>
+                    </a>
+            ))}
+        </div>
+      <Footer />
+    </>
+  );
+};
+
+export default Socials;
