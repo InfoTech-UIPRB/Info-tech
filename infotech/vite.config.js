eac04f8f66cd937a319f9caa435687e45bfa4f84
Edwin Vazquez Vega <120763821+Revokeez@users.noreply.github.com>
2025-05-10 14:10:11 -0300

    Modify vite file

--- infotech/vite.config.js
+++ infotech/vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Info-tech/",
+  base: "/info-tech/",
   plugins: [react(), tailwindcss()],
 })
