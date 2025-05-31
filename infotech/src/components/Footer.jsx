d8d620ed033d7d479368dce465cf5876872bbb12
Edwin Vazquez Vega <120763821+Revokeez@users.noreply.github.com>
2025-05-31 10:51:21 -0300

    Added icons in Footer component

--- infotech/src/components/Footer.jsx
+++ infotech/src/components/Footer.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import InfotechLogo from "../assets/Images/png_logo_infotech.png"
+import { InstagramIcon, GithubIcon } from "lucide-react";
 
 const Footer = () => {
     return (
-        <footer className="text-gray-300 py-10 relative top-auto w-full bottom-0 left-0 right-0">
+        <footer className="text-gray-300 py-10 relative y-10 w-full mt-auto">
           <div className="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8">
             <div className="flex flex-wrap justify-between">
               {/* <!-- Logo and Description --> */}
-              <div className=" md:w-1/3 sm:w-1/2 max-sm:w-16 mb-6 md:mb-0">
+              <div className="mb-6 md:mb-0">
                 <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
                   <img src={InfotechLogo} alt="InfoTech Logo" className="h-12" />
                   <span className="md:text-2xl text-xl font-bold text-white">InfoTech</span>
@@ -15,13 +16,11 @@ const Footer = () => {
               </div>
       
               {/* <!-- Social Media --> */}
-              <div className="lg:w-1/9 md:w-1/7 sm:w-1/4 w-1/3">
+              <div>
                 <h2 className="md:text-xl text-lg font-semibold text-white mb-4">Follow Us</h2>
-                <div className="flex space-x-4 max-sm:text-sm">
-                  <a href="https://www.instagram.com/infotech.uiprb/" target="_blank" className="text-white hover:text-yellow"><i
-                      className="fab fa-instagram"></i> Instagram</a>
-                  <a href="https://github.com/InfoTech-UIPRB" target="_blank" className="text-white hover:text-yellow"><i
-                      className="fab fa-github"></i> GitHub</a>
+                <div className="space-x-4 max-sm:text-sm flex flex-col space-y-2">
+                  <a href="https://www.instagram.com/infotech.uiprb/" target="_blank" className="text-white hover:text-yellow flex flex-row gap-x-1"> <InstagramIcon />Instagram</a>
+                  <a href="https://github.com/InfoTech-UIPRB" target="_blank" className="text-white hover:text-yellow flex flex-row gap-x-1"><GithubIcon /> GitHub</a>
                 </div>
               </div>
             </div>
