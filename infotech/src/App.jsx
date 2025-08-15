f50661a6eedbfdd383f65e5fedb8c6c882a6514a
Edwin Vazquez Vega <120763821+Revokeez@users.noreply.github.com>
2025-08-15 11:41:09 -0300

    Added GroupList and Sudebar Component

--- infotech/src/App.jsx
+++ infotech/src/App.jsx
@@ -6,6 +6,8 @@ import Page404 from "./pages/404Page.jsx";
 import Achievement from "./pages/Achievement.jsx"
 import Activity from "./pages/Activity.jsx";
 import Calendar from "./pages/Calendar.jsx";
+import Login from "./pages/LogIn.jsx";
+import DashBoard from "./pages/Dashboard.jsx";
 
 function App() {
   return (
@@ -16,6 +18,8 @@ function App() {
         <Route path="/achievement" element={<Achievement />}/>
         <Route path="/activity" element={<Activity />} />
         <Route path="/calendar" element={<Calendar />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/Dashboard" element={<DashBoard />} />
         <Route path="*" element={<Page404 />} />
       </Routes>
     </RouterProvider>
